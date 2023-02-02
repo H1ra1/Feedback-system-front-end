94f449274cd3dd90e3c9b7a39299c803cfe3aef1
Lira <hlira96@gmail.com>
2023-02-02 12:57:51 -0300

    add: SimpleHolder component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,20 @@
-import TinyHolder from '../../components/dashboard/holders/TinyHolder';
-import { FaAccessibleIcon } from 'react-icons/fa';
+import { MdAllInbox } from 'react-icons/md';
+import TinyHolder from '@/components/dashboard/holders/TinyHolder';
+import SimpleHolder from '@/components/dashboard/holders/SimpleHolder';
 
 function Dashboard() {
     return (
         <div>
             <section className='flex flex-wrap flex-gap-20'>
-                <TinyHolder icon={<FaAccessibleIcon />} title='Título do block' subtitle='Subtitulo do bloco' description='Descritivo do bloco opcional.'/>
+                <TinyHolder icon={<MdAllInbox />} title='Título do block' subtitle='Subtitulo do bloco' description='Descritivo do bloco opcional.'/>
                 <TinyHolder title='Título do block' subtitle='Subtitulo do bloco' description='Descritivo do bloco opcional.'/>
                 <TinyHolder title='Título do block' subtitle='Subtitulo do bloco' description='Descritivo do bloco opcional.'/>
                 <TinyHolder title='Título do block' subtitle='Subtitulo do bloco' description='Descritivo do bloco opcional.'/>
+            </section>
+
+            <section className='flex m-t-20 flex-gap-20'>
+                <SimpleHolder sizeClasses='col-xl col-xl-9' icon={<MdAllInbox />} mainTitle='Grupos de avaliações' subTitle='Últimos grupos cadastrados'/>
+                <SimpleHolder sizeClasses='col-xl col-xl-3' icon={<MdAllInbox />} mainTitle='Rank' subTitle='Usuários mais bem avaliados'/>
             </section>
         </div>
     );
---
+++ src/components/dashboard/holders/SimpleHolder/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+interface SimpleHolderProps {
+  sizeClasses?: string;
+  supTitle?: string;
+  subTitle?: string;
+  mainTitle?: string;
+  icon?: JSX.Element;
+}
+
+function SimpleHolder( props: SimpleHolderProps ) {
+  return (
+    <div className={`${styles['simple-holder']} default-shadow ${ props.sizeClasses != undefined ? props.sizeClasses : ''}`}>
+        <div className={`${styles['simple-holder__header']}`}>
+            <div className={`${styles['title-holder']}`}>
+                <p className={`${styles['aux-title']}`}>{props.supTitle}</p>
+                <div className={`${styles['main-title-holder']} flex flex-align-center`}>
+                    {props.icon}
+                    <p className={`${styles['main-title']}`}>{props.mainTitle}</p>
+                </div>
+                <p className={`${styles['aux-title']}`}>{props.subTitle}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SimpleHolder;

b4f325f05ddfb4d8aab79665e142fffec2e71aa2
Lira <hlira96@gmail.com>
2023-05-23 12:26:24 -0300

    fix: Redirect to login or dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 async function Home() {
-    const  SESSION                      = await getServerSession( authOptions );
+    const  SESSION = await getServerSession( authOptions );
 
-    if( ! SESSION ) {
+    if( ! SESSION || ! SESSION.user?.data ) {
         redirect( '/api/auth/signin' );
     } else {
         redirect( '/dashboard' );

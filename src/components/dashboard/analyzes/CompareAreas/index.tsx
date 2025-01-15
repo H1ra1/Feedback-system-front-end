2f559adab075f17f9bc528352a394323efbd26f3
H1ra1 <hlira96@gmail.com>
2025-01-15 12:21:32 -0300

    add: include compare areas filter tag

--- src/components/dashboard/analyzes/CompareAreas/index.tsx
+++ src/components/dashboard/analyzes/CompareAreas/index.tsx
@@ -1,98 +1,101 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
 import SortFullTable from '@/components/dashboard/tables/SortFullTable';
+import { useEffect, useState } from 'react';
 import FeederLoading from '../../loadings/FeederLoading';
 import styles from './styles.module.scss';
 
 function CompareAreas() {
-    const [ tableHeader, setTableHeader ] = useState( [] );
-    const [ tableBody, setTableBody ]     = useState( [] );
-    const [ loading, setLoading ]         = useState< boolean >( true );
+    const [tableHeader, setTableHeader] = useState([]);
+    const [tableBody, setTableBody] = useState([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect( () => {
+    useEffect(() => {
         async function getAreasCompare() {
-            setLoading( true );
+            setLoading(true);
 
-            const RESPONSE = await fetch( `${process.env.NEXT_PUBLIC_API_BASE}/analysis/areas-compare-per-questions/1T23/` );
-        
-            if( ! RESPONSE.ok )
-                throw new Error( RESPONSE.statusText );
-        
+            const RESPONSE = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/analysis/areas-compare-per-questions/1T23/`);
+
+            if (!RESPONSE.ok)
+                throw new Error(RESPONSE.statusText);
+
             const RESPONDE_PARSED = await RESPONSE.json();
 
-            if( RESPONDE_PARSED.data ) {
-                const HEADER = RESPONDE_PARSED.data.header.map( ( header: any ) => {
+            if (RESPONDE_PARSED.data) {
+                const HEADER = RESPONDE_PARSED.data.header.map((header: any) => {
                     return {
                         order: true,
                         item: header
                     }
-                } );
-                
+                });
 
-                const BODY = RESPONDE_PARSED.data.body.map( ( body: any ) => {
+
+                const BODY = RESPONDE_PARSED.data.body.map((body: any) => {
                     const OBJ: any = {
                         items: []
                     }
-                    body.forEach( ( value: any ) => {
-                        OBJ.items.push( {
+                    body.forEach((value: any) => {
+                        OBJ.items.push({
                             item: value
-                        } );
-                    } )
+                        });
+                    })
 
                     return OBJ;
-                } );
+                });
 
-                setTableHeader( HEADER );
-                setTableBody( BODY );
-                setLoading( false );
+                setTableHeader(HEADER);
+                setTableBody(BODY);
+                setLoading(false);
             }
         }
 
         getAreasCompare();
-    }, [] );
+    }, []);
 
-    async function filterByTag( tag: string ) {
-        setLoading( true );
+    async function filterByTag(tag: string) {
+        setLoading(true);
 
-        const RESPONSE = await fetch( `${process.env.NEXT_PUBLIC_API_BASE}/analysis/areas-compare-per-questions/${tag}/` );
-    
-        if( ! RESPONSE.ok )
-            throw new Error( RESPONSE.statusText );
-    
+        const RESPONSE = await fetch(`${process.env.NEXT_PUBLIC_API_BASE}/analysis/areas-compare-per-questions/${tag}/`);
+
+        if (!RESPONSE.ok)
+            throw new Error(RESPONSE.statusText);
+
         const RESPONDE_PARSED = await RESPONSE.json();
 
-        if( RESPONDE_PARSED.data ) {
-            const BODY = RESPONDE_PARSED.data.body.map( ( body: any ) => {
+        if (RESPONDE_PARSED.data) {
+            const BODY = RESPONDE_PARSED.data.body.map((body: any) => {
                 const OBJ: any = {
                     items: []
                 }
-                body.forEach( ( value: any ) => {
-                    OBJ.items.push( {
+                body.forEach((value: any) => {
+                    OBJ.items.push({
                         item: value
-                    } );
-                } )
+                    });
+                })
 
                 return OBJ;
-            } );
+            });
 
-            setTableBody( BODY );
-            setLoading( false );
+            setTableBody(BODY);
+            setLoading(false);
         }
     }
 
-    return ( 
+    return (
         <>
-            <div className={`${styles[ 'filters-holder' ]}`}>
-                <select name="filter-tag" id="filter-tag" onChange={ ( event ) => filterByTag( event.target.value ) }>
+            <div className={`${styles['filters-holder']}`}>
+                <select name="filter-tag" id="filter-tag" onChange={(event) => filterByTag(event.target.value)}>
                     <option value="1T23">1T23</option>
                     <option value="2T23">2T23</option>
                     <option value="3T23">3T23</option>
                     <option value="4T23">4T23</option>
+                    <option value="1T24">1T24</option>
+                    <option value="2T24">2T24</option>
+                    <option value="2S24">2S24</option>
                 </select>
             </div>
 
-            { loading ? <FeederLoading /> : <SortFullTable thead={ tableHeader } tbody={ tableBody } footer={ true } /> }
+            {loading ? <FeederLoading /> : <SortFullTable thead={tableHeader} tbody={tableBody} footer={true} />}
         </>
     );
 }
